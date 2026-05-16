e7f53f5ea9a442400f9a61fc69e9ecc5d172cbb7
Lamk-S <kunlancelot@gmail.com>
2026-05-16 16:00:13 -0500

    Add sigla column to presentaciones table

--- database/migrations/2026_05_15_173028_create_presentaciones_table.php
+++ database/migrations/2026_05_15_173028_create_presentaciones_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('presentaciones', function (Blueprint $table) {
             $table->id();
             $table->foreignId('caracteristica_id')->constrained('caracteristicas')->onDelete('cascade');
+            $table->string('sigla', 5);
             $table->timestamps();
         });
     }
