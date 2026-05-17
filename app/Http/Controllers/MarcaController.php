c4c48bef768d6cc863136fa30d212d9ba0ea517a
Lamk-S <kunlancelot@gmail.com>
2026-05-17 12:37:42 -0500

    feat: implement controllers and validation logic for inventory modules

---
+++ app/Http/Controllers/MarcaController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCaracteristicaRequest;
+use App\Http\Requests\UpdateMarcaRequest;
+use App\Models\Caracteristica;
+use App\Models\Marca;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class MarcaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $marcas = Marca::with('caracteristica')->latest()->get();
+        return view('marca.index', compact('marcas'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('marca.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCaracteristicaRequest $request)
+    {
+        try {
+            DB::beginTransaction();
+            $caracteristica = Caracteristica::create($request->validated());
+            $caracteristica->marca()->create([]);
+            DB::commit();
+        }catch (Exception $e){
+            DB::rollBack();
+        }
+
+        return redirect()->route('marcas.index')->with('success', 'Marca registrada');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        return view('marca.edit', compact('marca'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMarcaRequest $request, Marca $marca)
+    {
+        $marca->caracteristica->update($request->validated());
+
+        return redirect()->route('marcas.index')->with('success', 'Marca editada');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $message = '';
+        $marca = Marca::find($id);
+        if ($marca->caracteristica->estado == 1) {
+            Caracteristica::where('id', $marca->caracteristica->id)
+            ->update([
+                'estado' => 0
+            ]);
+            $message = 'Marca eliminada';
+        } else {
+            Caracteristica::where('id', $marca->caracteristica->id)
+            ->update([
+                'estado' => 1
+            ]);
+            $message = 'Marca restaurada';
+        }
+
+        return redirect()->route('marcas.index')->with('success', $message);
+    }
+}
