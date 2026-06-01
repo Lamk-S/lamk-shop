75e63d64090458c9ed3d407e61ab01f4820c2561
Lamk-S <kunlancelot@gmail.com>
2026-06-01 15:23:57 -0500

    feat: implement comprobante schema, model, and base seeders

--- database/seeders/ComprobanteSeeder.php
+++ database/seeders/ComprobanteSeeder.php
@@ -10,17 +10,18 @@ class ComprobanteSeeder extends Seeder
     public function run(): void
     {
         $comprobantes = [
-            ['tipo_comprobante' => 'Boleta', 'serie' => 'B001', 'correlativo_actual' => 0],
-            ['tipo_comprobante' => 'Factura', 'serie' => 'F001', 'correlativo_actual' => 0],
+            ['tipo_comprobante' => 'Boleta',  'uso_comprobante' => 'VENTA',  'serie' => 'BOLVENT', 'correlativo_actual' => 0],
+            ['tipo_comprobante' => 'Boleta',  'uso_comprobante' => 'COMPRA', 'serie' => 'BOLCOMP', 'correlativo_actual' => 0],
+            ['tipo_comprobante' => 'Factura', 'uso_comprobante' => 'VENTA',  'serie' => 'FACVENT', 'correlativo_actual' => 0],
+            ['tipo_comprobante' => 'Factura', 'uso_comprobante' => 'COMPRA', 'serie' => 'FACCOMP', 'correlativo_actual' => 0],
         ];
 
         foreach ($comprobantes as $item) {
             Comprobante::firstOrCreate(
+                ['serie' => $item['serie']],
                 [
                     'tipo_comprobante' => $item['tipo_comprobante'],
-                    'serie' => $item['serie'],
-                ],
-                [
+                    'uso_comprobante' => $item['uso_comprobante'],
                     'correlativo_actual' => $item['correlativo_actual'],
                     'estado' => 1,
                 ]
