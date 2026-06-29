bbb83da137f87fd36598447daa18ceee5701568c
Lamk-S <kunlancelot@gmail.com>
2026-06-29 14:03:58 -0500

    chore(build): configure vite server for network hmr and cors

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,19 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
     server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
+        host: '0.0.0.0',
+        port: 5173,
+        cors: true,
+        hmr: {
+            host: '10.246.240.25',
         },
     },
+
+    plugins: [
+        laravel({
+            input: ['resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
 });
